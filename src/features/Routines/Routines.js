3163f0a3fca73c8d6801a040d468b9be87ab8a1f
Cristian <cmioveanu@gmail.com>
2021-03-16 15:52:42 +0000

    add exercises for routines

--- src/features/Routines/Routines.js
+++ src/features/Routines/Routines.js
@@ -13,6 +13,8 @@ export const Routines = () => {
     const dispatch = useDispatch();
 
     const routines = useSelector(state => state.routines.routinesList);
+    const [exercisesRoutines, setExercisesRoutines] = useState([]);
+
     const [newRoutineName, setNewRoutineName] = useState("");
     const [routineToEdit, setRoutineToEdit] = useState(null);
 
@@ -22,7 +24,6 @@ export const Routines = () => {
         const getRoutines = async () => {
             const routines = await fetch("http://localhost:8080/myRoutines");
             const jsonRoutines = await routines.json();
-            console.log(jsonRoutines);
 
             dispatch(getRoutinesList(jsonRoutines));
             dispatch(changeActiveRoutine(jsonRoutines[0]));
@@ -30,6 +31,19 @@ export const Routines = () => {
 
         getRoutines();
     }, [dispatch]);
+
+
+    //get the exercises that belong to routines
+    useEffect(() => {
+        const getExercisesRoutines = async () => {
+            const exercisesRoutines = await fetch("http://localhost:8080/myRoutines/exercises");
+            const jsonExercisesRoutines = await exercisesRoutines.json();
+
+            setExercisesRoutines(jsonExercisesRoutines);
+        };
+
+        getExercisesRoutines();
+    }, [])
 
 
     //change new Routine name when typing
@@ -68,6 +82,11 @@ export const Routines = () => {
     }
 
 
+    //display exercises for each routine
+    const displayExercises = (routineID) => {
+        return exercisesRoutines.filter(exercise => exercise.routine_id === routineID);
+    }
+
     return (
         <section className={styles.routines}>
             <div className={styles.createNew}>
@@ -84,7 +103,7 @@ export const Routines = () => {
 
             {   /* Map Routines from global state for display */
                 routines.map(routine => (
-                    <div key={routine.id}>
+                    <div key={routine.id} className={styles.routineContainer}>
                         <div className={styles.individualRoutines}>
                             <h2>{routine.name}</h2>
                             <div>
@@ -93,6 +112,12 @@ export const Routines = () => {
                                 <button className={styles.editButton} onClick={() => handleEditClick(routine)}>Edit</button>
                             </div>
                         </div>
+
+                        {/* For each routine, display the corresponding exercises.
+                            Filter by routine ID */}
+                        {displayExercises(routine.id).map(exercise => (
+                            <p key={exercise.id} className={styles.individualExercises}>{exercise.name}</p>
+                        ))}
                     </div>
                 ))
             }
