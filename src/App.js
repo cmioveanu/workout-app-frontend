6d52c1b621a60c3e05bebc3947d13cb9a0048ea4
Cristian <cmioveanu@gmail.com>
2021-03-17 12:41:47 +0000

    move exercises fetching to App component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 import { Header } from './features/Header/Header';
 import { Exercises } from './features/Exercises/Exercises';
@@ -9,7 +11,28 @@ import { ExerciseHistory } from './features/ExerciseHistory/ExerciseHistory';
 import { Routines } from './features/Routines/Routines';
 import { RoutineHistory } from './features/RoutinesHistory/RoutineHistory';
 
+import { getExercisesList, changeActiveExercise } from './features/Exercises/ExercisesSlice';
+
 function App() {
+  const dispatch = useDispatch();
+
+  /* Get exercises and set first index as active element when app mounts. 
+     This needs to be done in the App component
+     because both exercises and routines pages depend on it.
+  */
+  useEffect(() => {
+    const getExercises = async () => {
+      const exercises = await fetch("http://localhost:8080/myExercises");
+      const jsonExercises = await exercises.json();
+
+      dispatch(getExercisesList(jsonExercises));
+      dispatch(changeActiveExercise(jsonExercises[0]));
+    };
+
+    getExercises();
+  }, [dispatch]);
+
+
   return (
     <BrowserRouter>
       <div className="App">
