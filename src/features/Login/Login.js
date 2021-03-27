163ef305444206c671d69c5189223f8bb21b6319
Cristian <cmioveanu@gmail.com>
2021-03-27 13:02:17 +0000

    add basic login routes and components

--- src/features/Login/Login.js
+++ src/features/Login/Login.js
@@ -4,17 +4,20 @@ import { Link } from 'react-router-dom';
 
 
 export const Login = () => {
+    const handleSubmit = () => {
+        fetch('http://localhost:8080/login', {
 
-
+        });    
+    }
 
     return (
-        <section className={styles.loginContainer}>
+        <section className={styles.loginContainer} action="/login" method="post">
             <form>
-                <label>Username:</label>
-                <input type="text"></input>
-                <label>Password:</label>
-                <input type="password"></input>
-                <button>Login</button>
+                <label htmlFor="username">Username:</label>
+                <input type="text" name="username" id="username"></input>
+                <label htmlFor="password">Password:</label>
+                <input type="password" name="password" id="password"></input>
+                <input type="submit" className={styles.submitButton} value="Log In"/>
             </form>
             <p>Don't have an account? <Link to="/register">Register here.</Link></p>
         </section >
