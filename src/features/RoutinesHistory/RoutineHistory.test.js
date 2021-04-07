c6fcb559aabe6914539b13dd439ed2594198ac9b
Cristian <cmioveanu@gmail.com>
2021-04-07 11:46:50 +0100

    add routines history test

--- src/features/RoutinesHistory/RoutineHistory.test.js
+++ src/features/RoutinesHistory/RoutineHistory.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 
 import { Provider } from 'react-redux';
 import store from '../../app/store';
@@ -22,6 +22,11 @@ test('renders history for active routine', async () => {
     await waitFor(() => {
         expect(screen.getByText('Bodyweight Pro')).toBeInTheDocument();
         expect(screen.getByText('29 March 2021')).toBeInTheDocument();
+        
+        const TUL = screen.getAllByText('Time under load:');
+        const negatives = screen.getAllByText('Negatives:');
+        expect(TUL.length).toBe(4);
+        expect(negatives.length).toBe(4);
     });
 });
 
